b7d6d83dcb4769b1ca0e00057d45bea74978f4d1
Doohwancho <doohwancho526@gmail.com>
2023-01-23 21:44:49 +0900

    C-b-3: higher order function explained on registry.js

--- javascript/frameless-frontend/01.render/03.component-registry/registry.js
+++ javascript/frameless-frontend/01.render/03.component-registry/registry.js
@@ -15,7 +15,7 @@ const renderWrapper = component => { //root node의 clone을 받아서 자식 DO
         const child = registry[name]
         if (!child) return
 
-        target.replaceWith(child(target, state)) //data-component에서 key가 매치되는 것만 찾아 state를 update
+        target.replaceWith(child(target, state)) //child가 renderWrapper(targetElement, state)가 된어 재귀적으로 동작
       })
 
     return element
@@ -23,9 +23,42 @@ const renderWrapper = component => { //root node의 clone을 받아서 자식 DO
 }
 
 //레지스트리 접근자(Accessor) 메서드
-//example) registry.add('todos', todosView)
+
+
+//registry.add('todos', todosView) 로 todosView를 넘겨주는데,
+
+//todosView) 아래 (name, component)에서 component 부분
+
+// const getTodoElement = todo => {
+//   const {
+//     text,
+//     completed
+//   } = todo
+
+//   return `
+//       <li ${completed ? 'class="completed"' : ''}>
+//         <div class="view">
+//           <input
+//             ${completed ? 'checked' : ''}
+//             class="toggle"
+//             type="checkbox">
+//           <label>${text}</label>
+//           <button class="destroy"></button>
+//         </div>
+//         <input class="edit" value="${text}">
+//       </li>`
+// }
+
+// export default (targetElement, { todos }) => {
+//   const newTodoList = targetElement.cloneNode(true)
+//   const todosElements = todos
+//     .map(getTodoElement)
+//     .join('')
+//   newTodoList.innerHTML = todosElements
+//   return newTodoList
+// }
 const add = (name, component) => {
-  registry[name] = renderWrapper(component)
+  registry[name] = renderWrapper(component) //registry['todos'] = todos' html
 }
 
 
