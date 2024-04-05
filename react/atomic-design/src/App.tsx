8da3331f143418d8e3f1ecde8394221afc9b2292
Doohwancho <doohwancho1993@gmail.com>
2024-04-05 18:48:44 +0900

    E-i-3. atomic design: radio

--- react/atomic-design/src/App.tsx
+++ react/atomic-design/src/App.tsx
@@ -1,18 +1,19 @@
 // import { useState } from "react";
 import "./App.css";
 import { Button } from "./atomic-design/atom/button";
+import Radio from "./atomic-design/atom/radio/Radio";
 import TextArea from "./atomic-design/atom/textarea/TextArea";
 
 function App() {
   return (
     <>
-      {/* step1 - button */}
+      <h2>step1 - Button</h2>
       <Button variant="solid" color="blue-lighten1" size="lg">
         Custom Button!
       </Button>
       <Button></Button>
 
-      {/* step2 - textarea */}
+      <h2>step2 - TextArea</h2>
       <TextArea
         appearance="standard" //style related, from useStyle.ts, - standard, none
         color="grey-base" //style related, from useStyle.ts
@@ -21,6 +22,34 @@ function App() {
         resize="none" //text related, from useTextarea.ts - horizontal, vertical, none
         defaultValue={"텍스트를 입력해주세요"} //text related, from useTextarea.ts
       ></TextArea>
+
+      <h2>step3 - Radio</h2>
+      <Radio
+        name="fruit"
+        size="lg"
+        color="yellow-lighten3"
+        defaultChecked={false}
+      >
+        포도
+      </Radio>
+      <Radio
+        name="fruit"
+        size="lg"
+        color="yellow-lighten3"
+        defaultChecked={true}
+        // onClick={(e) => alert(e.target.value)}
+      >
+        바나나
+      </Radio>
+      <Radio
+        name="fruit"
+        size="lg"
+        color="yellow-lighten3"
+        defaultChecked={true}
+        // onClick={(e) => alert(e.target.value)}
+      >
+        수박
+      </Radio>
     </>
   );
 }
---
+++ react/atomic-design/src/atomic-design/atom/radio/Radio.tsx
@@ -0,0 +1,24 @@
+/** @jsxImportSource @emotion/react */
+import { useRadio } from "./useRadio";
+import { useStyle } from "./useStyles";
+import type { RadioProps } from "./radioTypes";
+
+const Radio = (radioProps: RadioProps) => {
+  const { styleTheme } = useStyle(radioProps);
+  const { getInputProps, getPropsChild, disabled } = useRadio(radioProps);
+
+  return (
+    <label
+      css={[
+        styleTheme.defultRadioStyle(),
+        styleTheme.disabledRadioStyle(disabled),
+        styleTheme.radioSizeStyle(),
+      ]}
+    >
+      <input type="radio" {...getInputProps()} />
+      <span>{getPropsChild(radioProps.children)}</span>
+    </label>
+  );
+};
+
+export default Radio;
