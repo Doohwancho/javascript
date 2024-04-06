dff6b078af42a8bf1b2247cbe7c17f81574f473b
Doohwancho <doohwancho1993@gmail.com>
2024-04-06 17:08:17 +0900

    E-i-1. feat: theme's icons and uuid4 util function

--- react/atomic-design/src/atomic-design/atom/theme/index.ts
+++ react/atomic-design/src/atomic-design/atom/theme/index.ts
@@ -1,6 +1,16 @@
 import { variantPresetColors } from "./token/colors";
 import { getColorByToken, hexToRgba } from "./utils/color";
 import { HEX, ColorToken, Color } from "./token/colorTypes";
+import { uuidv4 } from "./utils/common";
+import { ClearableIcon, VisibleIcon, UnvisibleIcon } from "./icons/mixins";
 
-export { variantPresetColors, getColorByToken, hexToRgba };
+export {
+  variantPresetColors,
+  getColorByToken,
+  hexToRgba,
+  uuidv4,
+  ClearableIcon,
+  VisibleIcon,
+  UnvisibleIcon,
+};
 export type { ColorToken, Color, HEX };
