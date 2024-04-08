b0e5be0ce412056430b3d17324dd203a7530e390
Doohwancho <doohwancho1993@gmail.com>
2024-04-08 19:32:41 +0900

    E-i-atom-13. fix: list에서 ul/ol 앞에 numbering 안붙는 문제 해결

--- react/atomic-design/src/App.tsx
+++ react/atomic-design/src/App.tsx
@@ -1,5 +1,5 @@
 // import { useState } from "react";
-// import "./App.css";
+import "./App.css";
 import { Button } from "./atomic-design/atom/button";
 import Checkbox from "./atomic-design/atom/checkbox/checkbox";
 import Radio from "./atomic-design/atom/radio/Radio";
--- react/atomic-design/src/atomic-design/atom/list/TextList.tsx
+++ react/atomic-design/src/atomic-design/atom/list/TextList.tsx
@@ -7,9 +7,9 @@ interface TextItemProps {
   color?: string;
 }
 
+//list-style: auto;
 const TextItem: React.FC<TextItemProps> = ({ text, fontSize = 16, color = '#000' }) => {
   const textItemStyles = css`
-    list-style: auto;
     font-size: ${fontSize}px;
     color: ${color};
     margin-bottom: 8px;
@@ -25,24 +25,26 @@ interface TextListProps {
   className?: string;
 }
 
-// none, auto, disc, circle, square
-// list-style: auto;
-// list-style-type: auto; 
 const TextList: React.FC<TextListProps> = ({ items, fontSize, color, className = '' }) => {
   const textListStyles = css`
-    padding: 0;
-    margin: 0;
+    display: block;
+    list-style-type: decimal; /* none, decimal, disc, circle, square */
     ${className}
-  `;
 
+    /* ul/ol에 left margin이 없으면 numbering이 안됨 */
+    margin-block-start: 1em; 
+    margin-block-end: 1em;
+    margin-inline-start: 0px;
+    margin-inline-end: 0px;
+    padding-inline-start: 40px;
+  `;
   return (
     <>
-        {/* <ul css={textListStyles}> TODO - custom style을 넣으면 ul,ol의 numbering이 안붙는 문제*/}
-        <ul>
+        <ol css={textListStyles}> 
             {items.map((item, index) => (
                 <TextItem key={index} text={item} fontSize={fontSize} color={color} />
             ))}
-        </ul>
+        </ol>
     </>
   );
 };
